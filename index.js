cbaf5c5a1f69e6f0dccb83c1f6a8c37af22bc3d4
jhardiher giraldo <giraldojose814@gmail.com>
2023-04-23 21:38:15 -0500

    funcionalidad terminada

--- index.js
+++ index.js
@@ -1,28 +1,67 @@
-function areaEnCripatadora(){
+function encriptar() {
     let texto = document.getElementById("texto").value;
-    let tituloMensaje = document.getElementById(" tituloMensaje");
-    let imagen1 = document.getElementById("imagen1");
+    let tituloMensaje = document.getElementById("tituloMensaje");
     let parrafo = document.getElementById("parrafo");
-
-
-    let textoEncriptado = texto.replace(/a/gi,"ai")
-                        .replace(/e/gi,"enter")
-                        .replace(/i/gi,"imes")
-                        .replace(/o/gi,"ober")
-                        .replace(/u/gi,"ufat");
-                       
+    let imagen1 = document.getElementById("imagen1");
+  
+    let textoEncriptado = texto
+      .replace(/e/gi, "enter")
+      .replace(/i/gi, "imes")
+      .replace(/a/gi, "ai")
+      .replace(/o/gi, "ober")
+      .replace(/u/gi, "ufat");
+  
     if (texto.length != 0) {
-        texto = textoEncriptado;
-        tituloMensaje.textContent = "texto encriptado correctamente";
-        parrafo.textContent="";
-        imagen1.src ="./icons.imajenes/encriptado-de-datos.png desencriptar.png";
-
-    } else { 
-
-        imagen1.src = "./icons.imajenes/desencriptar.png";
-        alert("Debe ingreser algun texto valido");
-
+      document.getElementById("texto").value = textoEncriptado;
+      tituloMensaje.textContent = "Texto encriptado con éxito";
+      parrafo.textContent = "";
+      imagen1.src = "./icons,imajenes/encriptado-de-datos.png";
+    } else {
+      imagen1.src = "./icons,imajenes/encriptado-de-datos.png";
+      tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+      parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: '¡Debes ingresar un texto!',
+       
+      })
     }
+  }
+  
+  function Desencriptar() {
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("tituloMensaje");
+    let parrafo = document.getElementById("parrafo");
+    let imagen1= document.getElementById("imagen1");
+  
+    let textoEncriptado = texto
+      .replace(/enter/gi, "e")
+      .replace(/imes/gi, "i")
+      .replace(/ai/gi, "a")
+      .replace(/ober/gi, "o")
+      .replace(/ufat/gi, "u");
+    
+      if (texto.length != 0) {
+        document.getElementById("texto").value =  textoEncriptado;
+        tituloMensaje.textContent = "Texto desencriptado con éxito";
+        parrafo.textContent = "";
+        
+      imagen1.src = "./icons,imajenes/desifrado.png";
+      } else {
+        
+        imagen1.src = "./icons,imajenes/encriptado-de-datos.png";
 
+        tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+        parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+ 
 
-}
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: '¡Debes ingresar un texto!',
+           
+          })
+        
+      }
+  }
